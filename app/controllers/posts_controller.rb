6ab0de684d2eb78c820054b1682067e785c46aa4
Brandon <murray@cronusui.com>
2016-10-10 21:02:37 -0400

    Added before_action to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,14 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
     def index
         @posts = Post.all.order('created_at DESC')
     end
-    
+
     def new
         @post = Post.new
     end
-    
+
     def create
         @post = Post.new(post_params)
         if @post.save
@@ -15,36 +17,36 @@ class PostsController < ApplicationController
             render 'new'
         end
     end
-    
+
     def show
         @post = Post.find(params[:id])
     end
-    
+
     def edit
         @post = Post.find(params[:id])
     end
-    
+
     def update
         @post = Post.find(params[:id])
-        
+
         if @post.update(params[:post].permit(:title, :body))
             redirect_to @post
         else
             render 'edit'
         end
-        
+
     end
-    
+
     def destroy
         @post = Post.find(params[:id])
-        
+
         if @post.destroy
             redirect_to root_path
         end
     end
-    
+
     private
-    
+
         def post_params
             params.require(:post).permit(:title, :body)
         end
